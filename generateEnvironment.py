5861724e62819e3f554f200ed9294d81cc6c8b47
Sanskar Shah <sanzkarsha@gmail.com>
2022-09-18 15:49:20 -0400

    commented code

--- generateEnvironment.py
+++ generateEnvironment.py
@@ -1,57 +1,67 @@
+# Import the necessary ibraries
 import numpy as np
 from collections import deque 
 
 size = 51
 def generateMap(size = 51):
     """_summary_
-
+        Generate the size=51x51 block grid with 72% unblocked and 28% blocked cells
     Args:
-        size (int, optional): side of the square matrix to be made into a grid. Defaults to 51.
+        size (int, optional): Order of the square matrix to be made into a grid. Defaults to 51.
 
-    Returns: statusCode if grid was generated sucessfully along w the grid
+    Returns: StatusCode if grid was generated successfully along w the grid
         _type_: JSON
     """
+    # Initialize a 51x51 maze with random numbers
     grid = np.random.randint(100, size = (size, size))
     for i1, col in enumerate(grid):
         for i2, cell in enumerate(col):
+            # Set the block as unblocked for 72% blocks
             if cell<72:
                 grid[i1,i2] = 0
+            # For the rest 28% set the block as blocked
             else:
                 grid[i1, i2] = -1
+    # Set values for the root and Goal node separately
     grid[0,0], grid[-1,-1] = 0, 10
     return {"statusCode": 200, "grid": grid}
-        
+
+# Function to check if a cell is to be visited or not      
 def isValid(a, b, size, visited, grid):
     """_summary_
-
+        Checking for boundary conditions and overall validity
     Args:
-        a (_type_): _description_
-        b (_type_): _description_
-        size (_type_): _description_
-        visited (_type_): _description_
-        grid (_type_): _description_
+        a (_type_): Integer index declaration
+        b (_type_): Integer index declaration
+        size (_type_): Order of the square matrix used for traversal.
+        visited (_type_): A list to append the visited Locations of the maze
+        grid (_type_): The different combinations of 51x51 square matrix/maze generated
 
-    Returns:
-        _type_: _description_
+    Returns: False based on the condition satisfaction of the degree of the maze & True if the location is visited or blocked
+        _type_: Boolean
     """
+    # If the cell lies out of bounds
     if (a < 0 or a > size-1) or (b < 0 or b > size-1):
         return False
-    
+
+    # If the cell is already visited
     if visited[a, b] or grid[a, b] == -1:
         return False
     
+    #Otherwise
     return True
 
 def BFS(grid, size = 51):
     """_summary_
-
+        Performing Breadth First Search to reach to the goal block location
     Args:
-        grid (_type_): _description_
-        size (int, optional): _description_. Defaults to 51.
+        grid (_type_): The different combinations of 51x51 square matrix/maze generated
+        size (int, optional): Order of the matrix/maze used. Defaults to 51.
 
-    Returns:
-        _type_: _description_
+    Returns: StatusCode if there is a successful way for the agent to reach its goal/ bottom-right node
+        _type_: json
     """
+    # Stores indices of the location of the maze cells
     visited = np.array([[False]*size]*size)
     childRow = [-1, 0, 1, 0]
     childCol = [0, 1, 0 ,-1]
@@ -61,15 +71,18 @@ def BFS(grid, size = 51):
     startX, startY = 0, 0
     goalX, goalY = size - 1, size - 1
     
+    # Mark the starting cell as visited and push it into the goal queue
     startQ.append([startX,startY])
     visited[startX, startY] = True
     #goalQ.append(grid[goalX, goalY])
     
+    # Iterate while the queue is not empty
     while startQ:
         x1,y1 = startQ.popleft()
         path.append([x1,y1])
         if grid[x1,y1] == 10:
             return {"statusCode":200, "path":path}
+        # Go to the adjacent blocks on the maze
         for i in range(4):
             childX = x1 + childRow[i]
             childY = y1 + childCol[i]
@@ -79,6 +92,7 @@ def BFS(grid, size = 51):
                 
     return {"statusCode": 400, "path":"route DNE"}
 
+#  Function to check if a generated grid is solvable or not
 def isMapValid(grid):
     """_summary_
 
@@ -93,8 +107,9 @@ def isMapValid(grid):
     elif BFS(grid)["statusCode"] == 400:
         return False
 
-def checkWorlds():
+def genWorlds():
     """_summary_
+        Generate multiple combinations of 51x51 block maze and verify if there is a possible route to reach the goal node for the agent  
     """
     world = list()        
     for i in range(10):
@@ -102,7 +117,130 @@ def checkWorlds():
         world.append(grid)
         print(world[-1], grid.shape)
         print(BFS(world[-1])["statusCode"])
-        
-#checkWorlds()
-    
 
+# Driver Code       
+#genWorlds()
+    
+# Import the necessary ibraries
+import numpy as np
+from collections import deque 
+
+size = 51
+def generateMap(size = 51):
+    """_summary_
+        Generate the size=51x51 block grid with 72% unblocked and 28% blocked cells
+    Args:
+        size (int, optional): Order of the square matrix to be made into a grid. Defaults to 51.
+
+    Returns: StatusCode if grid was generated successfully along w the grid
+        _type_: JSON
+    """
+    # Initialize a 51x51 maze with random numbers
+    grid = np.random.randint(100, size = (size, size))
+    for i1, col in enumerate(grid):
+        for i2, cell in enumerate(col):
+            # Set the block as unblocked for 72% blocks
+            if cell<72:
+                grid[i1,i2] = 0
+            # For the rest 28% set the block as blocked
+            else:
+                grid[i1, i2] = -1
+    # Set values for the root and Goal node separately
+    grid[0,0], grid[-1,-1] = 0, 10
+    return {"statusCode": 200, "grid": grid}
+
+# Function to check if a cell is to be visited or not      
+def isValid(a, b, size, visited, grid):
+    """_summary_
+        Checking for boundary conditions and overall validity
+    Args:
+        a (_type_): Integer index declaration
+        b (_type_): Integer index declaration
+        size (_type_): Order of the square matrix used for traversal.
+        visited (_type_): A list to append the visited Locations of the maze
+        grid (_type_): The different combinations of 51x51 square matrix/maze generated
+
+    Returns: False based on the condition satisfaction of the degree of the maze & True if the location is visited or blocked
+        _type_: Boolean
+    """
+    # If the cell lies out of bounds
+    if (a < 0 or a > size-1) or (b < 0 or b > size-1):
+        return False
+
+    # If the cell is already visited
+    if visited[a, b] or grid[a, b] == -1:
+        return False
+    
+    #Otherwise
+    return True
+
+def BFS(grid, size = 51):
+    """_summary_
+        Performing Breadth First Search to reach to the goal block location
+    Args:
+        grid (_type_): The different combinations of 51x51 square matrix/maze generated
+        size (int, optional): Order of the matrix/maze used. Defaults to 51.
+
+    Returns: StatusCode if there is a successful way for the agent to reach its goal/ bottom-right node
+        _type_: json
+    """
+    # Stores indices of the location of the maze cells
+    visited = np.array([[False]*size]*size)
+    childRow = [-1, 0, 1, 0]
+    childCol = [0, 1, 0 ,-1]
+    
+    startQ = deque()
+    path = deque()
+    startX, startY = 0, 0
+    goalX, goalY = size - 1, size - 1
+    
+    # Mark the starting cell as visited and push it into the goal queue
+    startQ.append([startX,startY])
+    visited[startX, startY] = True
+    #goalQ.append(grid[goalX, goalY])
+    
+    # Iterate while the queue is not empty
+    while startQ:
+        x1,y1 = startQ.popleft()
+        path.append([x1,y1])
+        if grid[x1,y1] == 10:
+            return {"statusCode":200, "path":path}
+        # Go to the adjacent blocks on the maze
+        for i in range(4):
+            childX = x1 + childRow[i]
+            childY = y1 + childCol[i]
+            if isValid(childX, childY, size, visited, grid):
+                startQ.append([childX, childY])  
+                visited[childX, childY] = True
+                
+    return {"statusCode": 400, "path":"route DNE"}
+
+#  Function to check if a generated grid is solvable or not
+def isMapValid(grid):
+    """_summary_
+
+    Args:
+        grid (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if BFS(grid)["statusCode"] == 200:
+        return True
+    elif BFS(grid)["statusCode"] == 400:
+        return False
+
+def genWorlds():
+    """_summary_
+        Generate multiple combinations of 51x51 block maze and verify if there is a possible route to reach the goal node for the agent  
+    """
+    world = list()        
+    for i in range(10):
+        grid = generateMap()["grid"]
+        world.append(grid)
+        print(world[-1], grid.shape)
+        print(BFS(world[-1])["statusCode"])
+
+# Driver Code       
+genWorlds()
+    
